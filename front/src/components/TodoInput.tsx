6720399a2bb8b9a0d2a050c60dbfed09d1d7bcb3
turtle601 <turtle601@naver.com>
2022-01-31 16:14:46 +0900

    feat: TodoInput 컴포넌트 기능 구현 #6 submit -> 추가 기능만

--- front/src/components/TodoInput.tsx
+++ front/src/components/TodoInput.tsx
@@ -1,31 +1,55 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
 
 import { useForm } from 'react-hook-form';
 
+import { todosState } from '../atoms/atom.todo';
+
+import { FormType } from '../types/type';
+
 const TodoInput = () => {
+  const [todos, setTodos] = useRecoilState(todosState);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+    setValue,
+  } = useForm<FormType>();
 
-  const onValid = (data: any) => {
-    console.log(data);
+  const submitTodo = (data: FormType): void => {
+    setTodos([...todos, { id: Date.now(), ...data }]);
+    setValue('text', '');
+    setValue('category', '');
   };
-
-  console.log(errors);
 
   return (
     <>
-      <form onSubmit={handleSubmit(onValid)}>
-        <input {...register('todo', { required: '할 일을 입력하세요' })} />
+      <form onSubmit={handleSubmit(submitTodo)}>
+        <input {...register('text', { required: '할 일을 입력하세요' })} />
         <input
           {...register('category', { required: '카테고리를 정해주세요' })}
         />
         <button type="submit">할 일 입력</button>
       </form>
-      <div>{errors?.todo?.message}</div>
+      <div>{errors?.text?.message}</div>
       <div>{errors?.category?.message}</div>
+
+      {todos.map(todo => {
+        return (
+          <ul>
+            <li>{todo.category}</li>
+          </ul>
+        );
+      })}
+
+      {todos.map(todo => {
+        return (
+          <ul>
+            <li>{todo.text}</li>
+          </ul>
+        );
+      })}
     </>
   );
 };
