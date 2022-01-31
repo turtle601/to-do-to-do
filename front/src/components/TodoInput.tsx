9b93014af34c2f39f58fe4f179f6427731da67ef
turtle601 <turtle601@naver.com>
2022-01-31 14:44:42 +0900

    feat: input 포맷 구현 #6

--- front/src/components/TodoInput.tsx
+++ front/src/components/TodoInput.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import { useForm } from 'react-hook-form';
+
 const TodoInput = () => {
-  return <>TodoInput</>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+
+  console.log(errors);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input {...register('todo', { required: '할 일을 입력하세요' })} />
+        <input
+          {...register('category', { required: '카테고리를 정해주세요' })}
+        />
+        <button type="submit">할 일 입력</button>
+      </form>
+      <div>{errors?.todo?.message}</div>
+      <div>{errors?.category?.message}</div>
+    </>
+  );
 };
 
 export default TodoInput;
--- front/src/components/TodoTemplate.tsx
+++ front/src/components/TodoTemplate.tsx
@@ -4,7 +4,6 @@ import { ChildrenProps } from '../types/type.props';
 const TodoTemplate = ({ children }: ChildrenProps) => {
   return (
     <>
-      <div>TodoTemplate</div>
       <div>{children}</div>
     </>
   );
